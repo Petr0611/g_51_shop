1cbee0aef9fb5b8ec45a9139bfb57a48c0a1291e
Petr0611 <Ciolacpp@gmail.com>
2025-09-01 03:02:28 +0200

    Первый коммит

---
+++ src/main/java/ait/shop/Shop51Application.java
@@ -0,0 +1,13 @@
+package ait.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop51Application.class, args);
+    }
+
+}
